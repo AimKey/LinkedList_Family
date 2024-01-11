d765fc925d3ace978252a737d8a634f14ebde74c
Mazl <nhanhem2ui@gmail.com>
2024-01-11 17:28:25 +0700

    It's was me Mazl

---
+++ src/Model/CircularLinkedList.java
@@ -0,0 +1,85 @@
+package Model;
+
+public class CircularLinkedList<T> {
+
+    private Node<T> head;
+    private Node<T> tail;
+    private Node<T> currentPlayer;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void enqueue(T data) {
+        Node<T> newNode = new Node<>(data, null, null);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+            newNode.next = head;
+            newNode.pre = tail;
+        } else {
+            newNode.pre = tail;
+            tail.next = newNode;
+            tail = newNode;
+            tail.next = head;
+            head.pre = tail;
+        }
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return null;
+        }
+
+        T data = head.dataOfNode;
+        if (head == tail) {
+            head = null;
+            tail = null;
+            currentPlayer = null; // Reset currentPlayer when queue becomes empty
+        } else {
+            head = head.next;
+            tail.next = head;
+            head.pre = tail;
+        }
+
+        // Update currentPlayer if it was pointing to the dequeued player
+        if (currentPlayer != null && currentPlayer.getDataOfNode().equals(data)) {
+            currentPlayer = head;
+        }
+
+        return data;
+    }
+
+    public void viewQueue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty(move to next player or add player)");
+            return;
+        }
+
+        Node<T> current = head;
+        System.out.println("Queue:");
+        do {
+            System.out.print(current.getDataOfNode() + " ");
+            current = current.getNext();
+        } while (current != head);
+        System.out.println();
+    }
+
+    public void moveToNextPlayer() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+
+        if (currentPlayer == null) {
+            // Set the first player as the current player
+            currentPlayer = head;
+        } else {
+            // Move to the next player
+            currentPlayer = currentPlayer.next;
+        }
+
+        System.out.println("Current player: " + currentPlayer.getDataOfNode());
+    }
+}
